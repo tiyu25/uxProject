6e33c180bf9d311cdbdcca3f13d58d49e40e664f
tiyu25 <tiyuuuu9@email.com>
2025-02-12 21:37:16 +0900

    [feat] React Router 기본 라우트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import MainPage from "./pages/MainPage"
+
 const App = (): React.JSX.Element => {
 	return (
-		<>
-    
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<MainPage />}/>
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
---
+++ src/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = (): React.JSX.Element => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Header;
