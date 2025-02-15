8abc18f37ab118ce354132409c2814998b16b8c0
cupnoodles37 <roopre48@daum.net>
2025-02-15 20:52:32 +0900

    BlogSection 레이아웃 생성

--- src/components/sections/BlogSection.tsx
+++ src/components/sections/BlogSection.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { BlogData } from "../../constants/BlogData";
+import Slider from "react-slick";
 
 function BlogSection() {
-  return <div>BlogSection</div>;
+  return (
+    <div className="section-wrap">
+      <div className="sec-top">
+        <div className="sec-tit-wrap">
+          <h2 className="sec-tit">LESS PLASTIC</h2>
+          <p className="sec-sub-tit">
+            #LESS PLASTIC. WE ARE FANTASTIC! 관련된 콘텐츠들을 함께 만나보세요.
+          </p>
+        </div>
+        <a href="https://stories.amorepacific.com/midcont" className="more-btn">
+          Read More
+        </a>
+      </div>
+      <div className="sec-cont-wrap">
+        <ul className="sec-cont-list">
+          {BlogData.map((BlogData, index) => (
+            <li>
+              <a href={BlogData.link} key={index} className="data-item">
+                <div className="data-img-wrap">
+                  <img
+                    src={BlogData.image}
+                    alt={BlogData.title}
+                    className="data-image"
+                  />
+                  <button className="btn">상세보기 +</button>
+                </div>
+                <div className="data-txt-wrap">
+                  <h3 className="data-title">{BlogData.title}</h3>
+                  <p className="data-info">{BlogData.category}</p>
+                </div>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default BlogSection;
---
+++ src/constants/BlogData.ts
@@ -0,0 +1,30 @@
+export interface BlogItem {
+  title: string;
+  category: string;
+  image: string;
+  link: string;
+}
+
+export const BlogData: BlogItem[] = [
+  {
+    title: "AMORE:CYCLE의 용기 내준 판타스틱 크루를 만나다",
+    category: "LESS Plastic",
+    image:
+      "https://stories.amorepacific.com/wp-content/uploads/2025/01/2501-ESG_main_mo.jpg",
+    link: "https://stories.amorepacific.com/%ec%95%84%eb%aa%a8%eb%a0%88%ed%8d%bc%ec%8b%9c%ed%94%bd-amorecycle%ec%9d%98-%ec%9a%a9%ea%b8%b0-%eb%82%b4%ec%a4%80-%ed%8c%90%ed%83%80%ec%8a%a4%ed%8b%b1-%ed%81%ac%eb%a3%a8%eb%a5%bc-%eb%a7%8c%eb%82%98/",
+  },
+  {
+    title: "우리 모두가 LESS PLASTIC! 하면 생기는 일?",
+    category: "LESS Plastic",
+    image:
+      "https://stories.amorepacific.com/wp-content/uploads/2024/12/AS-241209_ESG-490X600.jpg",
+    link: "https://stories.amorepacific.com/%ec%95%84%eb%aa%a8%eb%a0%88%ed%8d%bc%ec%8b%9c%ed%94%bd-%ec%9a%b0%eb%a6%ac-%eb%aa%a8%eb%91%90-less-plasitc-%ed%95%98%eb%a9%b4-%ec%83%9d%ea%b8%b0%eb%8a%94-%ec%9d%bc/",
+  },
+  {
+    title:
+      "LESS PLASTIC. WE ARE FANTASTIC! 아모레퍼시픽 플라스틱 절감 캠페인 시즌 2",
+    category: "LESS Plastic",
+    image: "https://stories.amorepacific.com/wp-content/uploads/2024/12/M3.gif",
+    link: "https://stories.amorepacific.com/%ec%95%84%eb%aa%a8%eb%a0%88%ed%8d%bc%ec%8b%9c%ed%94%bd-less-plastic-we-are-fantastic-%ec%ba%a0%ed%8e%98%ec%9d%b8-%ec%8b%9c%ec%a6%8c2/",
+  },
+];
