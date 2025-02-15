4579ac91161163fcaaea77dd0e79d4cf9bcb8f11
cupnoodles37 <roopre48@daum.net>
2025-02-15 21:09:49 +0900

    PressSection 레이아웃 생성

--- src/components/sections/PressSection.tsx
+++ src/components/sections/PressSection.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { PressData } from "../../constants/PressData";
 
 function PressSection() {
   return (
-    <div>PressSection</div>
-  )
+    <div className="section-wrap">
+      <div className="sec-top">
+        <div className="sec-tit-wrap">
+          <h2 className="sec-tit">PRESS</h2>
+          <p className="sec-sub-tit">아모레퍼시픽의 소식을 전합니다.</p>
+        </div>
+        <a href="https://stories.amorepacific.com/midcont" className="more-btn">
+          Read More
+        </a>
+      </div>
+      <div className="sec-cont-wrap">
+        <ul className="sec-cont-list">
+          {PressData.map((PressData, index) => (
+            <li>
+              <a href={PressData.link} key={index} className="data-item">
+                <div className="data-img-wrap">
+                  <img
+                    src={PressData.image}
+                    alt={PressData.title}
+                    className="data-image"
+                  />
+                  <button className="btn">상세보기 +</button>
+                </div>
+                <div className="data-txt-wrap">
+                  <p className="data-date">{PressData.date}</p>
+                  <h3 className="data-title">{PressData.title}</h3>
+                </div>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default PressSection
+export default PressSection;
---
+++ src/constants/PressData.ts
@@ -0,0 +1,51 @@
+export interface PressItem {
+  title: string;
+  date: string;
+  image: string;
+  link: string;
+}
+
+export const PressData: PressItem[] = [
+  {
+    title: "설화수, 노스텔지어와 함께 한국 고가구 전시 진행",
+    date: "2025.02.13",
+    image:
+      "https://stories.amorepacific.com/wp-content/uploads/2025/02/THUM-500X329-0724_652877.jpg",
+    link: "https://stories.amorepacific.com/%ec%95%84%eb%aa%a8%eb%a0%88%ed%8d%bc%ec%8b%9c%ed%94%bd-%ec%84%a4%ed%99%94%ec%88%98-%eb%85%b8%ec%8a%a4%ed%85%94%ec%a7%80%ec%96%b4%ec%99%80-%ed%95%a8%ea%bb%98-%ed%95%9c%ea%b5%ad-%ea%b3%a0%ea%b0%80",
+  },
+  {
+    title: "아모레퍼시픽재단, 장원(粧源) 인문학자 5기 지원사업 공모",
+    date: "2025.02.11",
+    image:
+      "https://stories.amorepacific.com/wp-content/uploads/2025/02/THUM-500X329-0724_438d6c.jpg",
+    link: "https://stories.amorepacific.com/%ec%95%84%eb%aa%a8%eb%a0%88%ed%8d%bc%ec%8b%9c%ed%94%bd-%ec%95%84%eb%aa%a8%eb%a0%88%ed%8d%bc%ec%8b%9c%ed%94%bd%ec%9e%ac%eb%8b%a8-%ec%9e%a5%ec%9b%90%e7%b2%a7%e6%ba%90-%ec%9d%b8%eb%ac%b8",
+  },
+  {
+    title: "바이탈뷰티 슈퍼레티놀C, 일본 K-이너뷰티 시장서 인기",
+    date: "2025.02.10",
+    image:
+      "https://stories.amorepacific.com/wp-content/uploads/2025/02/THUM-500X329-0724-2.jpg",
+    link: "https://stories.amorepacific.com/%ec%95%84%eb%aa%a8%eb%a0%88%ed%8d%bc%ec%8b%9c%ed%94%bd-%eb%b0%94%ec%9d%b4%ed%83%88%eb%b7%b0%ed%8b%b0-%ec%8a%88%ed%8d%bc%eb%a0%88%ed%8b%b0%eb%86%80c-%ec%9d%bc%eb%b3%b8-k-%ec%9d%b4%eb%84%88%eb%b7%b0",
+  },
+  {
+    title: "에스트라, '에이시카<sup>365</sup> 흔적진정세럼 pH4.5' 리뉴얼 출시",
+    date: "2025.02.10",
+    image:
+      "https://stories.amorepacific.com/wp-content/uploads/2025/02/THUM-500X329-0724-1-2.jpg",
+    link: "https://stories.amorepacific.com/%ec%95%84%eb%aa%a8%eb%a0%88%ed%8d%bc%ec%8b%9c%ed%94%bd-%ec%97%90%ec%8a%a4%ed%8a%b8%eb%9d%bc-%ec%97%90%ec%9d%b4%ec%8b%9c%ec%b9%b4365-%ed%9d%94%ec%a0%81%ec%a7%84%ec%a0%95%ec%84%b8%eb%9f%bc-ph4-5",
+  },
+  {
+    title: "아모레퍼시픽그룹 2024년 경영 성과",
+    date: "2025.02.06",
+    image:
+      "https://stories.amorepacific.com/wp-content/uploads/2025/02/THUM-500X329-0724_b14045.jpg",
+    link: "https://stories.amorepacific.com/%ec%95%84%eb%aa%a8%eb%a0%88%ed%8d%bc%ec%8b%9c%ed%94%bd-%ec%95%84%eb%aa%a8%eb%a0%88%ed%8d%bc%ec%8b%9c%ed%94%bd%ea%b7%b8%eb%a3%b9-2024%eb%85%84-%ea%b2%bd%ec%98%81-%ec%84%b1%ea%b3%bc",
+  },
+  {
+    title: "일리윤, 세라마이드 아토 라인 3세대 출시",
+    date: "2025.02.05",
+    image:
+      "https://stories.amorepacific.com/wp-content/uploads/2025/02/THUM-500X329-0724_4368b3.jpg",
+    link: "https://stories.amorepacific.com/%ec%95%84%eb%aa%a8%eb%a0%88%ed%8d%bc%ec%8b%9c%ed%94%bd-%ec%9d%bc%eb%a6%ac%ec%9c%a4-%ec%84%b8%eb%9d%bc%eb%a7%88%ec%9d%b4%eb%93%9c-%ec%95%84%ed%86%a0-%eb%9d%bc%ec%9d%b8-3%ec%84%b8%eb%8c%80-%ec%b6%9c",
+  },
+];
