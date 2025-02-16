7b4b31dad0fef99ced321d141d2610c2000eb586
min-miny <myp7410@gmail.com>
2025-02-16 22:37:03 +0900

    [Modify]AppRouter 서브페이지 연결

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,6 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import MainPage from "../pages/MainPage"
 import Header from "../components/layout/Header"
 import Footer from "../components/layout/Footer"
+// Pages
+import SubpgInspire from "../pages/SubpgInspire";
+import SubpgHeritage from "../pages/SubpgHeritage";
+import SubpgNewbeauty from '../pages/SubpgNewbeauty';
+import SubpgEsg from "../pages/SubpgEsg";
+import SubpgPress from "../pages/SubpgPress";
 
 const AppRouter = (): React.JSX.Element => {
 	return (
@@ -9,6 +15,11 @@ const AppRouter = (): React.JSX.Element => {
 			<Header />
 			<Routes>
 				<Route path="/" element={<MainPage />}/>
+				<Route path="/inspire" element={<SubpgInspire />}/>
+				<Route path="/heritage" element={<SubpgHeritage />}/>
+				<Route path="/newbeauty" element={<SubpgNewbeauty />}/>
+				<Route path="/esg" element={<SubpgEsg />}/>
+				<Route path="/press" element={<SubpgPress />}/>
 			</Routes>
 			<Footer />
 		</BrowserRouter>
